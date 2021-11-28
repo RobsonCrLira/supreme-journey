f793c0948203e2444b726dc004c767b3be231150
Robson Cristian de Lira <robson.crlira@hotmail.com>
2021-11-28 10:46:42 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,18 +4,12 @@ import { badRequest } from '../helpers/httpHelpers';
 
 class SignUpController {
 	handle(httpRequest: HttpRequest): HttpResponse {
-		if (!httpRequest.body.name) {
-			return badRequest(new MissingParamError('name'));
+		const requiredFields = ['name', 'email'];
+		for (const field of requiredFields) {
+			if (!httpRequest.body[field])
+				return badRequest(new MissingParamError(field));
 		}
-
-		if (!httpRequest.body.email) {
-			return badRequest(new MissingParamError('email'));
-		}
-
-		return {
-			statusCode: 0,
-			body: new MissingParamError('0'),
-		};
+		return badRequest(new MissingParamError(''));
 	}
 }
 
