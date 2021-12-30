482b47c8bea3901de6b7a2ad8fa234660d16b4cb
Robson Cristian de Lira <robso.wejam@gmail.com>
2021-12-30 17:00:01 -0300

    fix: refactor jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
-export default {
+module.exports = {
 	roots: ['<rootDir>/src/'],
 	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 	coverageDirectory: 'coverage',
