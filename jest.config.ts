7b5c1e3ff2b113bf624424bbfece1482c84eb0da
Robson Cristian de Lira <robso.wejam@gmail.com>
2021-12-30 16:10:48 -0300

    chore: add jest configs and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -4,11 +4,11 @@
  */
 
 export default {
-  roots:['<rootDir>/src/'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  testEnvironment: "jest-environment-node",
-  transform: {
-    '.+\\.ts$':'ts-jest'
-  },
+	roots: ['<rootDir>/src/'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'jest-environment-node',
+	transform: {
+		'.+\\.ts$': 'ts-jest',
+	},
 };
