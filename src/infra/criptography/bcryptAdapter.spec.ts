443a8a3b06db46600824f341353ecf5f710ed644
Robson Cristian de Lira <robso.wejam@gmail.com>
2022-01-02 16:32:23 -0300

    test: ensure BcryptAdapter throws if bcrypt thorws

--- src/infra/criptography/bcryptAdapter.spec.ts
+++ src/infra/criptography/bcryptAdapter.spec.ts
@@ -11,6 +11,7 @@ const salt = 12;
 const makeSut = (): BcryptAdapter => {
 	return new BcryptAdapter(salt);
 };
+
 describe('Bcrypt Adapter', () => {
 	test('Should call bcrypt with correct values', async () => {
 		const sut = makeSut();
@@ -23,5 +24,14 @@ describe('Bcrypt Adapter', () => {
 		const sut = makeSut();
 		const hash = await sut.encrypt('any_value');
 		expect(hash).toBe('hash');
+	});
+
+	test('Should throw if bcrypt throws', async () => {
+		const sut = makeSut();
+		jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+			throw new Error();
+		});
+		const promise = sut.encrypt('any_value');
+		expect(promise).rejects.toThrow();
 	});
 });
