d358354bfb64a7a7e8bca1367374ad6f90501fa9
Robson Cristian de Lira <robso.wejam@gmail.com>
2022-01-02 21:59:05 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/accountRepository/account.spec.ts
+++ src/infra/db/mongodb/accountRepository/account.spec.ts
@@ -5,6 +5,12 @@ describe('Account Mongo Repository', () => {
 	beforeAll(async () => {
 		await MongoHelper.connect(`${process.env.MONGO_URL}`);
 	});
+
+	beforeEach(async () => {
+		const accountCollection = await MongoHelper.getCollection('accounts');
+		await accountCollection.deleteMany({});
+	});
+
 	afterAll(async () => {
 		await MongoHelper.disconnect();
 	});
--- src/infra/db/mongodb/accountRepository/account.ts
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helper/mongoHelper';
 
 export class AccountMongoRepository implements AddAccountRepository {
 	async add(accountData: AddAccountModel): Promise<AccountModel> {
-		const accountCollection = MongoHelper.getCollection('account');
+		const accountCollection = MongoHelper.getCollection('accounts');
 		const result = await accountCollection.insertOne({ ...accountData });
 		const account = { ...accountData, id: result.insertedId.toHexString() };
 		return account;
