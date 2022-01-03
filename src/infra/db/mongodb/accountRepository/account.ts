c3b60576d00938559c7863843b730402d4bd318b
Robson Cristian de Lira <robso.wejam@gmail.com>
2022-01-02 21:35:34 -0300

    feat: ensure AccountMongoRepository return an account on sucess

---
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -0,0 +1,13 @@
+import { AddAccountRepository } from '../../../../data/protocols/addAccountRepository';
+import { AccountModel } from '../../../../domain/models/account';
+import { AddAccountModel } from '../../../../domain/usecase/addAccount';
+import { MongoHelper } from '../helper/mongoHelper';
+
+export class AccountMongoRepository implements AddAccountRepository {
+	async add(accountData: AddAccountModel): Promise<AccountModel> {
+		const accountCollection = MongoHelper.getCollection('account');
+		const result = await accountCollection.insertOne({ ...accountData });
+		const account = { ...accountData, id: result.insertedId.toHexString() };
+		return account;
+	}
+}
--- src/infra/db/mongodb/helper/mongoHelper.ts
+++ src/infra/db/mongodb/helper/mongoHelper.ts
@@ -1,14 +1,17 @@
-import { MongoClient } from 'mongodb';
+import { Collection, MongoClient } from 'mongodb';
 
 export const MongoHelper = {
-	client: null as MongoClient,
+	client: null as any as MongoClient,
+
 	async connect(uri: string): Promise<void> {
-		this.client = MongoClient.connect(uri, {
-			directConnection: true,
-		});
+		this.client = await MongoClient.connect(uri);
 	},
 
 	async disconnect(): Promise<void> {
 		await this.client.close();
+	},
+
+	getCollection(name: string): Collection {
+		return this.client.db().collection(name);
 	},
 };
