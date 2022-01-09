0d0f0cd69b7baf90b493c6a1daa488f080b296cd
Robson Cristian de Lira <robso.wejam@gmail.com>
2022-01-09 10:33:04 -0300

    refactor: connect to mongodb before integration tests

--- src/infra/db/mongodb/accountRepository/account.spec.ts
+++ src/infra/db/mongodb/accountRepository/account.spec.ts
@@ -7,16 +7,18 @@ describe('Account Mongo Repository', () => {
 	});
 
 	beforeEach(async () => {
-		const accountCollection = await MongoHelper.getCollection('accounts');
+		const accountCollection = MongoHelper.getCollection('accounts');
 		await accountCollection.deleteMany({});
 	});
 
 	afterAll(async () => {
 		await MongoHelper.disconnect();
 	});
+
 	const makeSut = (): AccountMongoRepository => {
 		return new AccountMongoRepository();
 	};
+
 	test('Should return an account on success', async () => {
 		const sut = makeSut();
 		const account = await sut.add({
