dfcfeb1138dc3a234afe5d13419d22609cd4c9f1
Robson Cristian de Lira <robso.wejam@gmail.com>
2022-01-15 16:05:12 -0300

    feat: ensure mongodb reconnect if client is down

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
 	roots: ['<rootDir>/src/'],
-	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	collectCoverageFrom: [
+		'<rootDir>/src/**/*.ts',
+		'!<rootDir>/src/main/**',
+		'!<rootDir>/src/**/*Protocols.ts',
+	],
 	coverageDirectory: 'coverage',
 	testEnvironment: 'jest-environment-node',
 	preset: '@shelf/jest-mongodb',
