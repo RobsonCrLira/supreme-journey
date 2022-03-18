23bc9bd5ce7d2e30121b1e3f26c07b430403ff49
Robson Cristian de Lira <robso.wejam@gmail.com>
2022-03-18 20:37:46 -0300

    test: ensure LoginController return 400 if no password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -13,4 +13,14 @@ describe('Login Controller', () => {
 			badRequest(new MissingParamError('email'))
 		);
 	});
+	test('Should return 400 if password is provided', async () => {
+		const sut = new LoginController();
+		const httpRequest = {
+			body: { email: 'any_email@email.com' },
+		};
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse).toEqual(
+			badRequest(new MissingParamError('password'))
+		);
+	});
 });
