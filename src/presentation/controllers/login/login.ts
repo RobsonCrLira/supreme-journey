9b267de702f6b03a8e9ac79539eacb0a57b225df
Robson Cristian de Lira <robso.wejam@gmail.com>
2022-03-19 18:59:39 -0300

    feat: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,11 +1,16 @@
+import { Authentication } from '../../../domain/usecase/authentication';
 import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest, serverError } from '../../helpers/httpHelpers';
+import { badRequest, ok, serverError } from '../../helpers/httpHelpers';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { EmailValidator } from '../signup/signupProtocols';
 
 export class LoginController implements Controller {
-	constructor(private readonly emailValidator: EmailValidator) {
+	constructor(
+		private readonly emailValidator: EmailValidator,
+		private readonly authentication: Authentication
+	) {
 		this.emailValidator = emailValidator;
+		this.authentication = authentication;
 	}
 
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -20,15 +25,20 @@ export class LoginController implements Controller {
 
 			const isValid = this.emailValidator.isValid(email);
 
+			if (!password) {
+				return new Promise((resolve) =>
+					resolve(badRequest(new MissingParamError('password')))
+				);
+			}
+
 			if (!isValid) {
 				return new Promise((resolve) =>
 					resolve(badRequest(new InvalidParamError('email')))
 				);
 			}
+			const token = await this.authentication.auth(email, password);
 
-			return new Promise((resolve) =>
-				resolve(badRequest(new MissingParamError('password')))
-			);
+			return ok(token);
 		} catch (error) {
 			return serverError(error);
 		}
