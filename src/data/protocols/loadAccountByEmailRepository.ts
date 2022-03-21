e2b551e75dab0d606a6bca81a2e5cc28c8aa0f28
Robson Cristian de Lira <robso.wejam@gmail.com>
2022-03-20 22:44:45 -0300

    feat: ensure DbAuthentication call LoadAccountByEmailRepository with correct email

---
+++ src/data/protocols/loadAccountByEmailRepository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../useCases/addAccount/dbAddAccountProtocols';
+
+export interface LoadAccountByEmailRepository {
+	load(email: string): Promise<AccountModel>;
+}
---
+++ src/data/useCases/authentication/dbAuthentication.ts
@@ -0,0 +1,18 @@
+import {
+	Authentication,
+	AuthenticationModel,
+} from '../../../domain/usecase/authentication';
+import { LoadAccountByEmailRepository } from '../../protocols/loadAccountByEmailRepository';
+
+export class DbAuthentication implements Authentication {
+	constructor(
+		private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+	) {
+		this.loadAccountByEmailRepository = loadAccountByEmailRepository;
+	}
+
+	async auth(authentication: AuthenticationModel): Promise<string | null> {
+		await this.loadAccountByEmailRepository.load(authentication.email);
+		return null;
+	}
+}
