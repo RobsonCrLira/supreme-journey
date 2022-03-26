36dbef7aad8bcfaf38d1565ef7cbd44b3177d3af
Robson Cr Lira <robson.crlira@hotmail.com>
2022-03-26 15:30:06 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/useCases/authentication/dbAuthentication.ts
+++ src/data/useCases/authentication/dbAuthentication.ts
@@ -3,6 +3,7 @@ import {
 	AuthenticationModel,
 } from '../../../domain/usecase/authentication';
 import { LoadAccountByEmailRepository } from '../../protocols/db/loadAccountByEmailRepository';
+import { UpdateAccessTokenRepository } from '../../protocols/db/updateAccessTokenRepository';
 import { HashComparer } from '../../protocols/encrypter/hashComparer';
 import { TokenGenerator } from '../../protocols/encrypter/tokenGenerator';
 
@@ -10,11 +11,13 @@ export class DbAuthentication implements Authentication {
 	constructor(
 		private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
 		private readonly hashComparer: HashComparer,
-		private readonly tokenGenerator: TokenGenerator
+		private readonly tokenGenerator: TokenGenerator,
+		private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
 	) {
 		this.loadAccountByEmailRepository = loadAccountByEmailRepository;
 		this.hashComparer = hashComparer;
 		this.tokenGenerator = tokenGenerator;
+		this.updateAccessTokenRepository = updateAccessTokenRepository;
 	}
 
 	async auth(authentication: AuthenticationModel): Promise<string | null> {
@@ -29,6 +32,10 @@ export class DbAuthentication implements Authentication {
 			if (isValid) {
 				const accessToken = await this.tokenGenerator.generate(
 					account.id
+				);
+				await this.updateAccessTokenRepository.update(
+					account.id,
+					accessToken
 				);
 				return accessToken;
 			}
