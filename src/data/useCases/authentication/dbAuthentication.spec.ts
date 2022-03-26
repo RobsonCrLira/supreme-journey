0afbb0e70b9931d09d4bb4949bfd9df6106bbdce
Robson Cr Lira <robson.crlira@hotmail.com>
2022-03-26 15:07:23 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/useCases/authentication/dbAuthentication.spec.ts
+++ src/data/useCases/authentication/dbAuthentication.spec.ts
@@ -143,4 +143,10 @@ describe('DbAuthentication UseCase', () => {
 		const promise = sut.auth(makeFakeAuthentication());
 		expect(promise).rejects.toThrow();
 	});
+
+	test('Should returns a token on success', async () => {
+		const { sut } = makeSut();
+		const accessToken = await sut.auth(makeFakeAuthentication());
+		expect(accessToken).toBe('any_token');
+	});
 });
