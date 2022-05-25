3c9c235afa11e61497c53069615b03d930f571b6
Robson Cristian de Lira <robso.wejam@gmail.com>
2022-05-25 15:26:00 -0400

    feat: add login route

--- src/main/routes/loginRoutes.ts
+++ src/main/routes/loginRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { adaptRoute } from '../adapter/express/expressRouteAdapter';
+import { makeLoginController } from '../factories/login/loginFactory';
 import { makeSignUpController } from '../factories/signup/signupFactory';
 
 export default (router: Router): void => {
 	router.post('/signup', adaptRoute(makeSignUpController()));
+	router.post('/login', adaptRoute(makeLoginController()));
 };
