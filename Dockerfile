35394c8314d9286bd9dfdd7d9a6d22e2680fc243
Robson Cristian de Lira <robson.crlira@gmail.com>
2022-05-26 10:40:19 -0400

    refactor: change port and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:16.15-alpine
 WORKDIR /usr/app
 COPY ./package.json .
-RUN npm install --only=prod
+RUN  yarn --only=prod
 COPY ./dist ./dist
 EXPOSE 3000
-CMD npm start
+CMD yarn start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,6 +2,6 @@ export default {
 	mongoUrl:
 		process.env.MONGO_URL ||
 		'mongodb+srv://tdd-mongo:tddMongo@tdd-application.cfjbb.mongodb.net/tdd-application',
-	port: process.env.PORT || 3333,
+	port: process.env.PORT || 3000,
 	jwtSecret: process.env.JWT_SECRET || '8ee4ca1eca3442a7431efd8de1ebb7ec',
 };
