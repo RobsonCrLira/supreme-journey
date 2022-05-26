59c792a52c271e05e252fd9703d7461f78cb9821
Robson Cristian de Lira <robson.crlira@gmail.com>
2022-05-26 11:28:08 -0400

    chore: add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@ FROM node:16.15-alpine
 WORKDIR /usr/app
 COPY ./package.json .
 RUN  yarn --only=prod
-COPY ./dist ./dist
-EXPOSE 3000
-CMD yarn start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,5 @@
 export default {
-	mongoUrl:
-		process.env.MONGO_URL ||
-		'mongodb+srv://tdd-mongo:tddMongo@tdd-application.cfjbb.mongodb.net/tdd-application',
+	mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/tdd',
 	port: process.env.PORT || 3000,
 	jwtSecret: process.env.JWT_SECRET || '8ee4ca1eca3442a7431efd8de1ebb7ec',
 };
