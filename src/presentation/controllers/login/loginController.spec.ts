453e9c79363db25783e164ccc94c98eacee3f454
Robson Cr Lira <robson.crlira@hotmail.com>
2023-07-29 18:55:42 -0300

    test: ensure SignUpController return 500 if Authentication throwa

--- src/presentation/controllers/login/loginController.spec.ts
+++ src/presentation/controllers/login/loginController.spec.ts
@@ -84,6 +84,24 @@ describe('Login Controller', () => {
 		expect(httpResponse).toEqual(serverError(new Error()));
 	});
 
+	test('Should return 401 if invalid credentials are provided', async () => {
+		const { sut, authenticationStub } = makeSut();
+		jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(
+			new Promise((resolve) => resolve(null))
+		);
+		const httpResponse = await sut.handle(makeFakeRequest());
+		expect(httpResponse).toEqual(unauthorized());
+	});
+
+	test('Should return 500 if Authentication throws', async () => {
+		const { sut, authenticationStub } = makeSut();
+		jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(
+			new Promise((resolve, reject) => reject(new Error()))
+		);
+		const httpResponse = await sut.handle(makeFakeRequest());
+		expect(httpResponse).toEqual(serverError(new Error()));
+	});
+
 	test('Should return 200 valid credentials are provided', async () => {
 		const { sut } = makeSut();
 		const httpResponse = await sut.handle(makeFakeRequest());
